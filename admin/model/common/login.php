a70a97dfc3e3c170db37d36eb746b3fa38b541ee
heldersepu <heldersepu@6f948269-718f-6366-f943-c011a80894d1>
2014-06-25 12:11:48 +0000

    Dynamically create the table

--- admin/model/common/login.php
+++ admin/model/common/login.php
@@ -1,22 +1,26 @@
 <?php
 class ModelCommonLogin extends Model {
-	
+
 	public function getTotalIpChecks() {
 		function check_ip() {
- 			$ipaddress = '';
-		  	if (getenv('HTTP_CLIENT_IP'))
+			$ipaddress = 'UNKNOWN';
+			if (getenv('HTTP_CLIENT_IP'))
 				$ipaddress = getenv('HTTP_CLIENT_IP');
-		  	else if(getenv('REMOTE_ADDR'))
+			else if(getenv('REMOTE_ADDR'))
 				$ipaddress = getenv('REMOTE_ADDR');
-			else
-			        $ipaddress = 'UNKNOWN';
-			return $ipaddress;               
-		}      	
+			return $ipaddress;
+		}
 
 		$ip = check_ip();
-	
-		$query = $this->db->query("SELECT COUNT(*) AS total FROM `" . DB_PREFIX . "admin_ip_whitelist` WHERE ip = '" . $ip . "'");		 
+		$create_table = "CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "admin_ip_whitelist` ( " .
+			"  `admin_ip_whitelist_id` int(11) NOT NULL AUTO_INCREMENT, " .
+			"  `ip` varchar(15) COLLATE utf8_bin NOT NULL, " .
+			"  PRIMARY KEY (`admin_ip_whitelist_id`), " .
+			"  KEY `ip` (`ip`) " .
+			") ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ";
+		$this->db->query($create_table);
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM `" . DB_PREFIX . "admin_ip_whitelist` WHERE ip = '" . $ip . "'");
 		return $query->row['total'];
-	} 
+	}
 }
 ?>
